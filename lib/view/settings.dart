f5a4b73e4ce4178b71cc3c069bd73760845a7148
SuqenaSheikh <suqenasheikh@gmail.com>
2025-11-05 13:11:59 +0500

    settings screen modified

--- lib/view/settings.dart
+++ lib/view/settings.dart
@@ -12,10 +12,9 @@ class SettingsScreen extends StatelessWidget {
     final colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
-
       body: SafeArea(
         child: Padding(
-          padding:EdgeInsets.symmetric(horizontal: 20),
+          padding: EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
               SizedBox(height: size.height * 0.05),
@@ -29,45 +28,43 @@ class SettingsScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 8),
-             Expanded(
-                 child: Column(
-               children: [
-                 SizedBox(height: size.height * 0.05),
-                 _buildSettingsTile(
-                   context,
-                   icon: Icons.privacy_tip_outlined,
-                   title: 'Privacy Policy',
-                   onTap: () {
-                     Get.toNamed('/privacy');
-                   },
-                 ),
-                 _buildSettingsTile(
-                   context,
-                   icon: Icons.star_border_outlined,
-                   title: 'Rate Us',
-                   onTap: () {
-                     // Handle play store or app store redirection
-                   },
-                 ),
-                 _buildSettingsTile(
-                   context,
-                   icon: Icons.share_outlined,
-                   title: 'Share App',
-                   onTap: () {
-                     // Handle app sharing
-                   },
-                 ),
-                 _buildSettingsTile(
-                   context,
-                   icon: Icons.language_outlined,
-                   title: 'Language',
-                   onTap: () {
-                     Get.to(()=>SelectLanguageScreen());
-                   },
-                 ),
-               ],
-             ))
+              Column(
+                children: [
+                  SizedBox(height: size.height * 0.03),
+                  _buildSettingsTile(
+                    context,
+                    icon: Icons.privacy_tip_outlined,
+                    title: 'Privacy Policy',
+                    onTap: () {
+                      Get.toNamed('/privacy');
+                    },
+                  ),
+                  _buildSettingsTile(
+                    context,
+                    icon: Icons.star_border_outlined,
+                    title: 'Rate Us',
+                    onTap: () {
+                      // Handle play store or app store redirection
+                    },
+                  ),
+                  _buildSettingsTile(
+                    context,
+                    icon: Icons.share_outlined,
+                    title: 'Share App',
+                    onTap: () {
+                      // Handle app sharing
+                    },
+                  ),
+                  _buildSettingsTile(
+                    context,
+                    icon: Icons.language_outlined,
+                    title: 'Language',
+                    onTap: () {
+                      Get.to(() => SelectLanguageScreen());
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -75,26 +72,41 @@ class SettingsScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildSettingsTile(BuildContext context,
-      {required IconData icon, required String title, required VoidCallback onTap}) {
+  Widget _buildSettingsTile(
+    BuildContext context, {
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+  }) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return ListTile(
-      leading: Icon(icon, color: colorScheme.primary),
-      title: Text(
-        title,
-        style: textTheme.bodyMedium?.copyWith(
-          fontWeight: FontWeight.w500,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.onSecondary,
+          borderRadius: BorderRadius.circular(16),
+
+        ),
+        child: ListTile(
+          leading: Icon(icon, color: colorScheme.primary),
+          title: Text(
+            title,
+            style: textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w500),
+          ),
+          trailing: Icon(
+            Icons.arrow_forward_ios,
+            size: 18,
+            color: colorScheme.primary,
+          ),
+          onTap: onTap,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 7),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          tileColor: Theme.of(context).scaffoldBackgroundColor,
         ),
       ),
-      trailing: Icon(Icons.arrow_forward_ios, size: 18, color: colorScheme.primary),
-      onTap: onTap,
-      contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 4),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      tileColor: Theme.of(context).scaffoldBackgroundColor,
     );
   }
 }
